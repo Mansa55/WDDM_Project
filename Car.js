f51d472edc5ab2dd23467aa4fffa844a3137c775
Mansa55 <114674867+Mansa55@users.noreply.github.com>
2023-01-08 17:52:42 -0500

    Add files via upload

---
+++ Car.js
@@ -0,0 +1,17 @@
+class Car {
+    constructor( make, _model, _year, _color){
+     this.make = _make;
+     this.model = _model;
+     this.year = _year;
+     this.color = _color;
+    }
+}
+
+var make = "Honda";
+var model = "Civic";
+var year = "2019";
+var color = "Silver";
+function changeColor(_color) {
+    this.color = _color;
+}
+  
---
+++ MainScript.js
@@ -0,0 +1,52 @@
+// How to create variables:
+var x;
+let y;
+
+// How to use variables:
+x = 5;
+y = 6;
+let z = x + y;
+
+//let z = x + x;
+
+// a const variable cannot be reassigned
+
+const PI = 3.141592653589793
+
+//PI = 10; // this code will not work
+
+console.log("PI: " + PI);
+
+// const variables must be assigned a value
+// this code will not work
+//const PI2;
+//PI2 = 3.14159265359;
+
+// always use const when you know the value should not be changed
+// using came1Case is the most common way of naming variables
+
+var firstName = "Brahim";
+var lastName = "Kallon";
+
+let fullName = firstName + " " + lastName;
+
+console.log("First Name: " + firstName + "Last Name: " + lastName);
+console.log("Full Name " + fullName);
+
+console.log("X: " + x + "\n" + "Y: " + y + "\n" + "Z: " + z);
+
+console.log("this is" + " a " + "combined string with an integer: " + z);
+let incrementNum = 0;
+
+console.log("the number: " + incrementNum);
+
+incrementNum++;
+
+console.log("the incremented number: " + incrementNum);
+
+function myClickFunction() {console.log("The button was clicked!!!");}
+
+const myCar = new Car ("Mini", "Cooper", "2023", "Red");
+console.log("MY CAR\nMAKE: " + myCar.make + "\nMODEL: " + myCar.model + 
+"\nYEAR: " + myCar.year + "\nCOLOR: " + myCar.color);
+myCar.changeColor("Black");
